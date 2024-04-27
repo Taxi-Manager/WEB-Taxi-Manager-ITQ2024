6cf546499646495179db94332a8439bde57fe78f
unbreakrermario <98780285+unbreakrermario@users.noreply.github.com>
2024-04-27 15:48:06 -0600

    modulo crear conduntores y eliminar , editar y actualizar
    creamos el front de los 2 con angular y ya es visible

--- Taxi-Manager/src/app/app.component.ts
+++ Taxi-Manager/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import {CrearConductorComponent} from './compGestionConductores/crear-conductor/crear-conductor.component'
+import {EditDeleteUpdateComponent} from './compGestionConductores/edit-delete-update/edit-delete-update.component'
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CrearConductorComponent,EditDeleteUpdateComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
