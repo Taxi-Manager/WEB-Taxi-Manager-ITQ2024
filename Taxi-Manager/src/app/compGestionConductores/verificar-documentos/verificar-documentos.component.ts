cb9fb266f14a1547730958e1bcf858d5590bd41f
Aaronxd10 <117193713+Aaronxd10@users.noreply.github.com>
2024-05-03 16:24:30 -0600

    Actuazlizacion de pantalla verificar-documentos

--- Taxi-Manager/src/app/compGestionConductores/verificar-documentos/verificar-documentos.component.ts
+++ Taxi-Manager/src/app/compGestionConductores/verificar-documentos/verificar-documentos.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
-
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-verificar-documentos',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './verificar-documentos.component.html',
   styleUrl: './verificar-documentos.component.css'
 })
 export class VerificarDocumentosComponent {
 
+  verificacionDocumentosVisible: boolean = false;
+
+  mostrarDocumentos() {
+    this.verificacionDocumentosVisible = true;
+  }
+
+  cerrarVerificacion() {
+    this.verificacionDocumentosVisible = false;
+  }
+
+  cargarDocumentos() {
+    // Lógica para cargar los documentos verificados
+  }
 }
