b84d003cde334e8dbd0a45af32f0a63222d09fbb
Aaronxd10 <117193713+Aaronxd10@users.noreply.github.com>
2024-05-06 21:03:35 -0600

    Modificacion de pantalla, verificar documentos, implemente nuevo ccs ya establecido en el daily

--- Taxi-Manager/src/app/compGestionConductores/verificar-documentos/verificar-documentos.component.ts
+++ Taxi-Manager/src/app/compGestionConductores/verificar-documentos/verificar-documentos.component.ts
@@ -22,4 +22,20 @@ export class VerificarDocumentosComponent {
   cargarDocumentos() {
     // Lógica para cargar los documentos verificados
   }
+  public nombresArchivos: { [id: string]: string } = {}; // Objeto para almacenar nombres de archivos (clave: ID, valor: nombre)
+
+  mostrarNombre(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const files = target.files;
+    const inputId = target.id; // Obtener el ID del input
+
+    if (files && files.length > 0) {
+      const fileName = files[0].name;
+      this.nombresArchivos[inputId] = fileName; // Actualizar el objeto con el nombre del archivo y el ID del input
+    } else {
+      this.nombresArchivos[inputId] = ''; // Eliminar el nombre del archivo si no se seleccionó ninguno
+    }
+  }
+
 }
+
