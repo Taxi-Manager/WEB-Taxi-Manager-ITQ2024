fdb24c2128d9155a85a420f55224dd7f49219de4
Jesús Chávez Arias <l19141132@queretaro.tecnm.mx>
2024-05-20 13:10:27 -0600

    Generacion de dos coches 'Taxi'
    se general dos coches con ID defentes en la pantalla Garaje 3D

--- Taxi-Manager/src/app/componentes3D/pantalla3-d/pantalla3-d.component.ts
+++ Taxi-Manager/src/app/componentes3D/pantalla3-d/pantalla3-d.component.ts
@@ -1,5 +1,4 @@
-import { NONE_TYPE } from '@angular/compiler';
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, Input, OnInit, Renderer2 } from '@angular/core';
 import {
   Scene,
   Engine,
@@ -20,36 +19,39 @@ import "@babylonjs/loaders"
 @Component({
   selector: 'app-pantalla3-d',
   standalone: true,
-  imports: [],
   templateUrl: './pantalla3-d.component.html',
-  styleUrl: './pantalla3-d.component.css'
+  styleUrls: ['./pantalla3-d.component.css']
 })
-export class Pantalla3DComponent  {
-  canvas!: Nullable<HTMLCanvasElement>;
+export class Pantalla3DComponent implements OnInit {
+  @Input() canvasId!: string;
 
-  scene!:  Scene;
+  canvas!: Nullable<HTMLCanvasElement>;
+  scene!: Scene;
   engine!: Engine;
   camera!: ArcRotateCamera;
 
-  constructor(private render:Renderer2){}
+  constructor(private render: Renderer2) {}
 
-  ngOnInit():void {
-    this.canvas = this.render.selectRootElement('#canva3d')
-    this.engine = new Engine(this.canvas, true)
+  ngOnInit(): void {
+    console.log(`Initializing canvas with ID: ${this.canvasId}`);
+    // Usamos setTimeout para esperar a que el DOM esté completamente renderizado
+    setTimeout(() => {
+      this.canvas = document.getElementById(this.canvasId) as HTMLCanvasElement;
+      console.log(`Canvas element:`, this.canvas);
+      this.engine = new Engine(this.canvas, true);
 
-    this.scene = this.CreateScene();
-    this.CreateCamera();
-    this.CreateBox();
+      this.scene = this.CreateScene();
+      this.CreateCamera();
+      this.CreateBox();
 
-    //Ejecutar babylonJS
-
-    this.engine.runRenderLoop(() =>{
-      this.scene.render();
-    });
+      // Ejecutar BabylonJS
+      this.engine.runRenderLoop(() => {
+        this.scene.render();
+      });
+    }, 0);
   }
 
   CreateCamera(): void {
-
     this.camera = new ArcRotateCamera(
       "camera",
       0, 0, 10,
@@ -57,7 +59,7 @@ export class Pantalla3DComponent  {
       this.scene
     );
 
-    this.camera.attachControl(this.canvas, false);
+    this.camera.attachControl(this.canvas, true);
     this.camera.setPosition(new Vector3(0, 0, 75));
 
     this.camera.wheelPrecision = 100;
@@ -68,16 +70,13 @@ export class Pantalla3DComponent  {
 
     this.camera.useAutoRotationBehavior = true;
     this.camera.useFramingBehavior = true;
-    this.camera.fov = 5.3
+    this.camera.fov = 5.3;
   }
 
   CreateScene(): Scene {
-    const scene2 = new Scene(this.engine);
     const scene = new Scene(this.engine);
-    
     const envTex = CubeTexture.CreateFromPrefilteredData(
       "./assets/images/sky.env",
-      //"./assets/images/fondo.png",
       scene
     );
 
@@ -87,40 +86,28 @@ export class Pantalla3DComponent  {
     scene.createDefaultSkybox(envTex, true);
     scene.environmentIntensity = 1;
 
-    //return scene;
-    return scene2;
+    return scene;
   }
 
   async CreateBox(): Promise<void> {
-
-    //lights
     const light = new PointLight("Omni0", new Vector3(0.0, 0.0, 0.0), this.scene);
-    light.intensity = 0.2
-    
+    light.intensity = 0.2;
 
     SceneLoader.ImportMesh("", "./assets/", "taxi.gltf", this.scene, (newMeshes) => {
-
-      console.log(newMeshes)
-      //Meshes
-      const cubo_body = newMeshes[1]
+      console.log(`Meshes loaded for canvas ID: ${this.canvasId}`, newMeshes);
+      const cubo_body = newMeshes[1];
       cubo_body.rotation.x = Math.PI;
-      const cubo_tapa = newMeshes[2]
+      const cubo_tapa = newMeshes[2];
       cubo_tapa.rotation.x = Math.PI;
-      const letter_katavo = newMeshes[3]
+      const letter_katavo = newMeshes[3];
       letter_katavo.rotation.x = Math.PI;
-      const lightSphere= newMeshes[4]
+      const lightSphere = newMeshes[4];
 
+      light.parent = lightSphere;
 
-      light.parent = lightSphere
-
-      
-      
-
-      //Glow effect
       const gl = new GlowLayer("glow", this.scene);
       gl.intensity = 3;
       gl.customEmissiveColorSelector = (mesh, subMesh, material, result) => {
-
         if (mesh.name === "myLigth") {
           result.set(1, 0, 1, 0.5);
         } else {
@@ -128,8 +115,6 @@ export class Pantalla3DComponent  {
         }
       };
 
-    
-      //Shadows
       const shadowGenerator = new ShadowGenerator(512, light);
 
       shadowGenerator.getShadowMap()?.renderList?.push(cubo_tapa);
@@ -139,26 +124,22 @@ export class Pantalla3DComponent  {
       shadowGenerator.useKernelBlur = true;
       shadowGenerator.blurKernel = 64;
 
-      cubo_body.receiveShadows = true
-
-      //Animation groups
+      cubo_body.receiveShadows = true;
 
       const freezeAnim = this.scene.getAnimationGroupByName("Freeze");
       const moveAnim = this.scene.getAnimationGroupByName("Animating");
 
       this.scene.onPointerObservable.add((pointer) => {
         if (pointer.type == PointerEventTypes.POINTERPICK) {
-
           if (moveAnim?.isStarted) {
-            freezeAnim?.start(true, 1)
-            moveAnim?.stop()
-
+            freezeAnim?.start(true, 1);
+            moveAnim?.stop();
           } else {
-            moveAnim?.start(true,1)
-            freezeAnim?.stop()
+            moveAnim?.start(true, 1);
+            freezeAnim?.stop();
           }
         }
-      })
+      });
     });
-  };
+  }
 }
